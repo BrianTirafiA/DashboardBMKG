c4ad67778418dfbea212726b00f522a7cb13626d
ekarahmadi <septianekarahmadi@365.ugm.ac.id>
2025-01-07 10:20:06 +0700

    add layout

--- resources/views/components/navbar-all.blade.php
+++ resources/views/components/navbar-all.blade.php
@@ -4,9 +4,9 @@
     @endphp  
   
     <x-navbar :menu-items="[  
-        ['label' => 'QC Dashboard', 'link' => '/qcdashboard', 'active' => $currentUrl === url('/qcdashboard')],  
+        ['label' => 'AWS QC', 'link' => '/qcdashboard', 'active' => $currentUrl === url('/qcdashboard')],  
         ['label' => 'Server Room', 'link' => '/itasset/dashboard', 'active' => str_starts_with($currentUrl, url('/itasset/'))],  
-        ['label' => 'Lending Items', 'link' => '/lendasset/lendingitems', 'active' => $currentUrl === url('/lendasset/lendingitems')],  
+        ['label' => 'Lending Items', 'link' => '/lendasset/lendingitems', 'active' => str_starts_with($currentUrl, url('/lendasset/'))],  
         ['label' => 'Calendar', 'link' => '#', 'active' => $currentUrl === url('/calendar')],  
         ['label' => 'Reports', 'link' => '#', 'active' => $currentUrl === url('/reports')],  
     ]" />  

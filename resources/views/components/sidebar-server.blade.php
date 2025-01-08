ed0d8eabb7a0ab8bdba40e251d3c7ee9a76658f2
lhanif <lhanif467@gmail.com>
2025-01-08 15:07:38 +0700

    Fix : Route

--- resources/views/components/sidebar-server.blade.php
+++ resources/views/components/sidebar-server.blade.php
@@ -6,9 +6,9 @@
             [
                 'type' => 'static',
                 'title' => 'Dashboard',
-                'link' => '/itasset/dashboard',
+                'link' => '/admin/itasset/dashboard',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-5 h-5"> <path fill-rule="evenodd" d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z" clip-rule="evenodd"></path></svg>',
-                'active' => $currentRoute === 'itasset/dashboard', // Menandai aktif  
+                'active' => $currentRoute === 'admin/itasset/dashboard', // Menandai aktif  
             ],
             [
                 'type' => 'dropdown',
@@ -19,10 +19,10 @@
                             </svg>',
                 'active' => str_starts_with($currentRoute, url('/itasset/device')), // Kunci active untuk dropdown 
                 'items' => [
-                    ['title' => 'Server', 'link' => '/itasset/device/server', 'active' => $currentRoute === '/itasset/device/server'],
-                    ['title' => 'KVM', 'link' => '/itasset/device/kvm', 'active' => $currentRoute === '/itasset/device/kvm'],
-                    ['title' => 'UPS', 'link' => '/itasset/device/ups', 'active' => $currentRoute === '/itasset/device/ups'],
-                    ['title' => 'Fan', 'link' => '/itasset/device/fan', 'active' => $currentRoute === '/itasset/device/fan'],
+                    ['title' => 'Server', 'link' => '/admin/itasset/device/server', 'active' => $currentRoute === '/admin/itasset/device/server'],
+                    ['title' => 'KVM', 'link' => '/admin/itasset/device/kvm', 'active' => $currentRoute === '/admin/itasset/device/kvm'],
+                    ['title' => 'UPS', 'link' => '/admin/itasset/device/ups', 'active' => $currentRoute === '/admin/itasset/device/ups'],
+                    ['title' => 'Fan', 'link' => '/admin/itasset/device/fan', 'active' => $currentRoute === '/admin/itasset/device/fan'],
                 ],
             ],
             [
@@ -31,8 +31,8 @@
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hdd-rack-fill" viewBox="0 0 16 16">
                                                   <path d="M2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h1v2H2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2h-1V7h1a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm.5 3a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m2 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m-2 7a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m2 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1M12 7v2H4V7z"/>
                                                 </svg>',
-                'link' => '/itasset/rack',
-                'active' => $currentRoute === 'itasset/rack', // Menandai aktif  
+                'link' => '/admin/itasset/rack',
+                'active' => $currentRoute === 'admin/itasset/rack', // Menandai aktif  
             ],
             [
                 'type' => 'static',
@@ -43,8 +43,8 @@
                             <path d="M2 6h2.45L2.908 7.639A1.5 1.5 0 0 0 3.313 10H2zm8.595-2-.308 1H12a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9.276l-.942 1H12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"/>
                             <path d="M12 10h-1.783l1.542-1.639q.146-.156.241-.34zm0-3.354V6h-.646a1.5 1.5 0 0 1 .646.646M16 8a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8"/>
                             </svg>',
-                'link' => '/itasset/power',
-                'active' => $currentRoute === 'itasset/power', // Menandai aktif  
+                'link' => '/admin/itasset/power',
+                'active' => $currentRoute === 'admin/itasset/power', // Menandai aktif  
             ],
             [
                 'type' => 'static',
@@ -52,26 +52,26 @@
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-wide-connected" viewBox="0 0 16 16">
                             <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5m0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78zM5.048 3.967l-.087.065zm-.431.355A4.98 4.98 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8zm.344 7.646.087.065z"/>
                             </svg>',
-                'link' => '/itasset/maintenance',
-                'active' => $currentRoute === 'itasset/maintenance', // Menandai aktif  
+                'link' => '/admin/itasset/maintenance',
+                'active' => $currentRoute === 'admin/itasset/maintenance', // Menandai aktif  
             ],
             [
                 'type' => 'static',
                 'title' => 'Report',
-                'link' => '/itasset/report',
+                'link' => '/admin/itasset/report',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-check-fill" viewBox="0 0 16 16">  
                             <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m1.354 4.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708"/>  
                             </svg>',
-                'active' => $currentRoute === 'itasset/report', // Menandai aktif  
+                'active' => $currentRoute === 'admin/itasset/report', // Menandai aktif  
             ],  
             [
                 'type' => 'static',
                 'title' => 'Log',
-                'link' => '/itasset/log',
+                'link' => '/admin/itasset/log',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
                             </svg>',
-                'active' => $currentRoute === 'itasset/log', // Menandai aktif  
+                'active' => $currentRoute === 'admin/itasset/log', // Menandai aktif  
             ],
         ];  
     @endphp  

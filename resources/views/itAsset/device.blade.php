a56db388ee7ccaba59daf197b40206c2c5677fe7
lhanif <lhanif467@gmail.com>
2025-02-10 22:51:34 +0700

    Fix

--- resources/views/itAsset/device.blade.php
+++ resources/views/itAsset/device.blade.php
@@ -45,17 +45,6 @@
                     <div class="flex flex-col gap-2 shrink-0 me-5 sm:flex-row">
                         <form action="{{ route('device.index') }}" method="GET"
                             class="relative h-10 w-full min-w-[200px] bg-white">
-                            <input id="searchInput" name="search"
-                                class="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 text-sm text-blue-gray-700 outline-none transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 focus:border-2 focus:border-gray-900"
-                                placeholder="Cari perangkat" value="{{ request(key: 'search') }}" />
-                            <button type="submit" class="absolute right-3 top-2.5 text-blue-gray-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z">
-                                    </path>
-                                </svg>
-                            </button>
                         </form>
                         <button type="button" onclick="openAddModal()"
                             class="flex items-center gap-3 rounded-lg bg-gray-900 py-2 px-4 text-xs font-bold uppercase text-white shadow-md transition-all hover:shadow-lg">
@@ -66,7 +55,7 @@
 
                 <div class="ms-5 me-5 flex p-6 px-0 -mt-5 overflow-hidden">
                     <div class="w-full rounded-xl overflow-hidden border border-blue-gray-100">
-                        <div class="overflow-x-auto max-h-[500px] overflow-y-auto">
+                        <div class="overflow-x-auto max-h-screen overflow-y-auto mb-6">
                             <table id="deviceTable" class=" mt-4 text-left border-collapse table-auto min-w-full">
                                 <thead>
                                     <tr>
@@ -159,38 +148,6 @@
                         </div>
                     </div>
                 </div>
-
-                <div id="paginasi" class="flex items-center justify-between p-4 border-t border-blue-gray-50">
-                    <p class="block font-sans text-sm font-normal leading-normal text-blue-gray-900">
-                        Total Data: {{ $device->total }} | Page {{ $device->currentPage  }} of {{ $device->lastPage
-                        }}
-                    </p>
-                    <div class="flex gap-2">
-                        @if($device->onFirstPage)
-                        <span
-                            class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center font-sans text-xs font-bold uppercase text-gray-900 transition-all opacity-50 cursor-not-allowed">
-                            Previous
-                        </span>
-                        @else
-                        <a href="{{ $device->previousPageUrl. (request('search') ? '&search=' . urlencode(request('search')) : '') }}"
-                            class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75">
-                            Previous
-                        </a>
-                        @endif
-                        @if($device->hasMorePages)
-                        <a href="{{ $device->nextPageUrl . (request('search') ? '&search=' . urlencode(request('search')) : '') }}"
-                            class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75">
-                            Next
-                        </a>
-                        @else
-                        <span
-                            class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center font-sans text-xs font-bold uppercase text-gray-900 transition-all opacity-50 cursor-not-allowed">
-                            Next
-                        </span>
-                        @endif
-                    </div>
-                </div> 
-
             </div>
         </div>
     </div>
